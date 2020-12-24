d36eb2633b39057b10ea4ca29da10e268121e3df
tkadima <kadima.t@husky.neu.edu>
2020-12-23 19:58:59 -0500

    got my game board down

---
+++ general_assembly/project_1/app.js
@@ -0,0 +1,41 @@
+let movementDisplay = movement;
+
+game.setAttribute('width', getComputedStyle(game)['width'])
+game.setAttribute('height', getComputedStyle(game)['height'])
+
+let ctx = game.getContext('2d')
+
+function Crawler(x, y, color, width, height) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+    this.height = height
+    console.log('constructin')
+    this.alive = true
+    this.render = function () {
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        myGameArea.clear();
+        myGamePiece.newPos();
+        myGamePiece.update();
+    }
+}
+
+let player = new Crawler(60, 50, 'pink', 20, 20);
+
+new Date();
+
+let gameLoop = () => {
+    ctx.clearRect(0, 0, game.widht, game.height)
+    movementDisplay.innerText = `X: '${hero.x}\nY: ${hero.y}`
+    if (zombie.alive) {
+
+    zombie.render();
+    }
+    hero.render();
+}
+
+
