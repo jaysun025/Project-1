ed34466d7195d1ef63f2ee80fc8aceae3bbb013d
tkadima <kadima.t@husky.neu.edu>
2020-12-25 15:40:52 -0500

    trying to get survivor to move right and left

--- general_assembly/project_1/app.js
+++ general_assembly/project_1/app.js
@@ -1,16 +1,16 @@
 //SELECTORS
-
-// let movementDisplay = movement;
 let gameBoard = document.getElementById('game')
+let surviver = new Crawler(600, 630, 'pink', 20, 20);
+let zomboy = new Crawler(10, 10, 'gray', 20, 20);
+let ctx = gameBoard.getContext('2d')
 
 // define the game board(dom references)
-// make character appear on screen
+
 
 //EVENT CALLERS
 gameBoard.setAttribute('width', getComputedStyle(game)['width'])
 gameBoard.setAttribute('height', getComputedStyle(game)['height'])
 
-let ctx = gameBoard.getContext('2d')
 
 //FUNCTIONS
 
@@ -26,48 +26,35 @@ function Crawler(x, y, color, width, height) {
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    // this.newPos = function() {
-    //     myGameArea.clear();
-    //     myGamePiece.newPos();
-    //     myGamePiece.update();
-    // }
+
 }
 
-let surviver = new Crawler(600, 630, 'pink', 20, 20);
-let zomboy = new Crawler(0, 0, 'gray', 20, 20);
+function leftArrowPressed() {
+    var element = document.getElementById("survivor");
+    element.style.left = parseInt(element.style.left) - 5 + 'px';
+    }
+function rightArrowPressed() {
+    var element = document.getElementById("survivor");
+    element.style.left = parseInt(element.style.left) - 5 + 'px';
+    }
+
+    function moveSelection(evt) {
+        switch (evt.keyCode) {
+            case 37:
+            leftArrowPressed();
+            break;
+            case 39:
+            rightArrowPressed();
+            break;
+        }
+    }
 
 new Date();
 surviver.render();
 zomboy.render();
 
-let gameLoop = () => {
-    ctx.clearRect(0, 0, game.width, game.height)
-    if (zomboy.alive) {
-// use function to detect hit
-// call function is curly boys
-    }
-    // define function
-}
 
-let movementHandler = e => {
-    switch(e.key) {
-    case 'w':
-    //do some stuff
-    surviver.y -= 10
-    break
-    case 'a':
-     //move left
-    surviver.x -= 10
-    break
-    case 's':
-    // move down
-    surviver.y += 10
-    break
-    case 'd':
-    // move right
-    surviver.x += 10
-    break
-    deafault; 
-    console.log('that key has no purpose here')   
-            }
-        }
+
+
+
+
