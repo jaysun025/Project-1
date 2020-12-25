619859fde8a68036745e7d63de1f00678e2938a0
tkadima <kadima.t@husky.neu.edu>
2020-12-25 14:38:59 -0500

    moved survivor to starting position

--- general_assembly/project_1/app.js
+++ general_assembly/project_1/app.js
@@ -1,13 +1,18 @@
+//SELECTORS
+
 // let movementDisplay = movement;
 let gameBoard = document.getElementById('game')
 
 // define the game board(dom references)
 // make character appear on screen
 
+//EVENT CALLERS
 gameBoard.setAttribute('width', getComputedStyle(game)['width'])
 gameBoard.setAttribute('height', getComputedStyle(game)['height'])
 
 let ctx = gameBoard.getContext('2d')
+
+//FUNCTIONS
 
 function Crawler(x, y, color, width, height) {
     this.x = x
@@ -28,19 +33,41 @@ function Crawler(x, y, color, width, height) {
     // }
 }
 
-let surviver = new Crawler(60, 50, 'pink', 20, 20);
-let zomboy = new Crawler(0, 0, 'gray', 200, 200);
+let surviver = new Crawler(600, 630, 'pink', 20, 20);
+let zomboy = new Crawler(0, 0, 'gray', 20, 20);
 
 new Date();
 surviver.render();
 zomboy.render();
 
 let gameLoop = () => {
-    ctx.clearRect(0, 0, game.widht, game.height)
-    movementDisplay.innerText = `X: '${hero.x}\nY: ${hero.y}`
-    if (zombie.alive) {
-
+    ctx.clearRect(0, 0, game.width, game.height)
+    if (zomboy.alive) {
+// use function to detect hit
+// call function is curly boys
     }
+    // define function
 }
 
-
+let movementHandler = e => {
+    switch(e.key) {
+    case 'w':
+    //do some stuff
+    surviver.y -= 10
+    break
+    case 'a':
+     //move left
+    surviver.x -= 10
+    break
+    case 's':
+    // move down
+    surviver.y += 10
+    break
+    case 'd':
+    // move right
+    surviver.x += 10
+    break
+    deafault; 
+    console.log('that key has no purpose here')   
+            }
+        }
